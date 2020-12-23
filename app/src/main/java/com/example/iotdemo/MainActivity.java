82f60b206ce55771bc1011b4aa6b8bad553f1083
Duyen <phamduyen13799@gmail.com>
2020-12-23 09:29:30 +0700

    show data from firebase

--- app/src/main/java/com/example/iotdemo/MainActivity.java
+++ app/src/main/java/com/example/iotdemo/MainActivity.java
@@ -1,14 +1,86 @@
 package com.example.iotdemo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseDatabase database;
+    DatabaseReference mData;
+
+    ListView lvData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_devices);
+
+        database= FirebaseDatabase.getInstance();
+        mData = database.getReference();
+
+        lvData =  findViewById(R.id.listDevice);
+
+        // Cấu hình ListView
+        final ArrayList<String> mac = new ArrayList<String>();
+        final ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, mac);
+        lvData.setAdapter(adapter);
+
+        lvData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+                String mac_id =(String) adapter.getItem(position);
+                intent.putExtra("mac_device", mac_id);
+                startActivity(intent);
+            }
+        });
+
+        mData.child("Devices").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                mac.add(snapshot.getKey());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+//        Device device = new Device("Cam bien 2", "50:12:91:C2:H6:E3");
+//
+//        mData.child("Devices").child(device.getMac()).setValue(device.getName());
     }
 }
---
+++ app/src/main/java/com/example/iotdemo/SecondaryActivity.java
@@ -0,0 +1,80 @@
+package com.example.iotdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class SecondaryActivity extends AppCompatActivity {
+    private FirebaseDatabase database;
+    DatabaseReference mData;
+    private ProgressBar temperature;
+    private ProgressBar light;
+    private ProgressBar humidity;
+
+    private ImageButton turnOffLight;
+    private ImageButton turnOnLight;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent=getIntent();
+        String stringValue = intent.getStringExtra("mac_device");
+
+        database= FirebaseDatabase.getInstance();
+        mData = database.getReference();
+
+        temperature = findViewById(R.id.proBarTemperature);
+        light = findViewById(R.id.proBarLight);
+        humidity= findViewById(R.id.proBarHumidity);
+
+        turnOffLight = findViewById(R.id.imageButtonTurnOff);
+        turnOnLight = findViewById(R.id.imageButtonTurnOn);
+
+        mData.child("Devices").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Device device= snapshot.child(stringValue).getValue(Device.class);
+                    temperature.setProgress((int) Float.parseFloat(device.getTemperature()));
+                    light.setProgress((int) Float.parseFloat(device.getLight()));
+                    humidity.setProgress((int) Float.parseFloat(device.getHumidity()));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        turnOnLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SecondaryActivity.this, "Đã bật đèn", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        turnOffLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SecondaryActivity.this, "Đã tắt đèn", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
